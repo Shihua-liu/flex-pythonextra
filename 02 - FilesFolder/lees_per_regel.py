af0da3559890d4d11b81761fabf46f86243c5686
shihua <liushihua2004@gmail.com>
2020-10-01 15:30:51 +0200

    les 02 afmaken

---
+++ 02 - FilesFolder/lees_per_regel.py
@@ -0,0 +1,44 @@
+import os
+
+bestand = open("klasgenoten.txt", "r")
+
+tekst_regel = bestand.readline()
+
+
+while tekst_regel:
+
+    tekst_regel = bestand.readline()
+
+    print(tekst_regel)
+
+
+regel1 = bestand.readline()
+print(regel1)
+
+regel2 = bestand.readline()
+print(regel2)
+
+regel3 = bestand.readline()
+print(regel3)
+
+regel4 = bestand.readline()
+print(regel4)
+
+regel5 = bestand.readline()
+print(regel5)
+
+regel6 = bestand.readline()
+print(regel6)
+
+regel7 = bestand.readline()
+print(regel7)
+
+regel8 = bestand.readline()
+print(regel8)
+
+regel9 = bestand.readline()
+print(regel9)
+
+regel10 = bestand.readline()
+print(regel10)
+
